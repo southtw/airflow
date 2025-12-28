d95d3b2de06528db0eeec116b94a61172e587b07
southtw <109669438+southtw@users.noreply.github.com>
2025-12-28 20:49:51 +0800

    Add loader functionality with external scripts

---
+++ loader.lua
@@ -0,0 +1,6 @@
+local v0 = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/southtw/airflow/refs/heads/main/src/uilib.luau"))();
+local v1 = v0:DrawLoader();
+local v2 = v1:DrawGame({Name="Main",Logo=""});
+v2:DrawVersion({Name="Universal",Status=v1.Enum.StatusTypes.Online,LastUpdate="2025/12/28",LoadScript=function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/southtw/airflow/refs/heads/main/universal.lua"))();
+end});
