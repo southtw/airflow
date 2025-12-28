f7549863f606b1d09921eb0120be6aac3be72a29
southtw <109669438+southtw@users.noreply.github.com>
2025-12-28 23:03:06 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,3 +4,6 @@ local v2 = v1:DrawGame({Name="Main",Logo=""});
 v2:DrawVersion({Name="Universal",Status=v1.Enum.StatusTypes.Online,LastUpdate="2025/12/28",LoadScript=function()
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/southtw/airflow/refs/heads/main/universal.lua"))();
 end});
+v2:DrawVersion({Name="Frontlines",Status=v1.Enum.StatusTypes.Online,LastUpdate="2025/12/28",LoadScript=function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/southtw/airflow/refs/heads/main/game/frontlines.lua"))();
+end});
